b0da85683d3d0978c5b33fb87be9e2cb905efd18
Kevin S. Hahn <kevinshahn@gmail.com>
2015-06-04 20:46:38 -0700

    updates bootstrap script to use system python
    - fewer python things installed with macports

--- bootsrap.sh
+++ bootsrap.sh
@@ -7,61 +7,130 @@ today=$(date "+%Y_%m_%d")
 # OSX specific
 if [ $(uname) == Darwin ]; then
     # Check for Xcode
+    echo '- checking for Xcode.app...'
     if [ ! -s /Applications/Xcode.app ]; then
-        echo '[ERROR]  Xcode - NOT INSTALLED'
+        echo '++ [ERROR]  Xcode.app not installed'
         exit 1
     fi
 
     # Check for MacPorts
+    echo '- checking for macports...'
     if [ ! $(which port) ]; then
-        echo '[ERROR]  `port` command not found in PATH'
+        echo '++ [ERROR]  `port` command not in PATH'
         exit 1
     fi
 
     # Install vim, macvim, tmux, git-core and python stuff via MacPorts
+    echo '- installing ports (uses sudo):'
+    echo '-   macvim+huge+python27, tmux, tmux-pasteboard, git, curl, htop, tree'
     sudo port install \
-        vim +huge +python27 \
         macvim +huge +python27 \
         tmux tmux-pasteboard \
-        py27-flake8 \
         git \
         curl \
+        htop \
+        tree \
 
-    # easy install system pip
-    # easy install system virtualenv
-    # easy install system 
-
-    sudo port select --set pep8 pep827
-    sudo port select --set pyflakes py27-pyflakes
+    echo '- setting default python to python27-apple... (uses sudo)'
     sudo port select --set python python27-apple
-    sudo port select --set nosetests nosetests27
+
+    # get and install pip
+    echo '- checking for /usr/local/bin/pip'
+    if [ ! -e /usr/local/bin/pip ]; then
+        echo '++ downloading get-pip.py script'
+        curl -Sso /var/tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
+        echo '++ installing pip to system python... (uses sudo)'
+        sleep 1
+        sudo /usr/bin/python /var/tmp/get-pip.py
+    else
+        echo '++ exists. nothing to do'
+    fi
 fi
 
-# Make dot_vim directory
-[ ! -d $dir/dot_vim ] && mkdir -p $dir/dot_vim/{autoload,colors}
+echo '- checking for flake8'
+if [ ! -e /usr/local/bin/flake8 ]; then
+    echo '++ installing /usr/local/bin/flake8'
+    sudo pip install flake8==2.4.1
+else
+    echo '++ exists. nothing to do'
+fi
 
-# Make links
-ln -sf $dir $HOME/.dotfiles
+echo '- checking for ipython'
+if [ ! -e /usr/local/bin/ipython ]; then
+    echo '++ installing /usr/local/bin/ipython'
+    sudo pip install ipython
+else
+    echo '++exists. nothing to do'
+fi
+
+echo '- checking for virtualenv'
+if [ ! -e /usr/local/bin/virtualenv ]; then
+    echo '++ installing /usr/local/bin/virtualenv'
+    sudo pip install virtualenv
+else
+    echo '++exists. nothing to do'
+fi
+
+echo '- checking for virtualenvwrapper.sh'
+if [ ! -e /usr/local/bin/ipython ]; then
+    echo '++ installing /usr/local/bin/virtualenvwrapper.sh'
+    sudo pip install virtualenvwrapper
+else
+    echo '++exists. nothing to do'
+fi
+
+echo '- checking for vim plugin directory structure'
+if [ ! -d $dir/dot_vim ]; then
+    echo '++ creating vim plugin directory structure'
+    mkdir -p $dir/dot_vim/{autoload,colors}
+else
+    echo '++ exists. nothing to do...'
+fi
+
+echo '- checking for ~/.dotfiles directory'
+if [ ! -d $HOME/.dotfiles ]; then
+    echo '++ soft-linking repo to ~/.dotfiles'
+    ln -sf $dir $HOME/.dotfiles
+else
+    echo '++ exists. nothing to do...'
+fi
+
+echo '- checking ~/.* files and symlink'
 for file in $(ls $dir | grep '^dot_'); do
     dotfile=$(sed s'/^dot_/\./' <<< $file)
-    mv $HOME/$dotfile $HOME/$dotfile.$today.bak
+    if [ -a $HOME/$dotfile ]; then
+        # mv $HOME/$dotfile $HOME/$dotfile.$today.bak
+        rm $HOME/$dotfile
+    fi
+    echo "++ ln -sf $dir/$file to ~/$dotfile"
     ln -sf $dir/$file $HOME/$dotfile
+    if [ -a ~/.dotfiles/dot_vim/.vim ]; then
+        rm ~/.dotfiles/dot_vim/.vim
+    fi
 done
 
 # Make directory for virtualenvs
-[ ! -d !/VirtualEnvs ] && mkdir -p ~/VirtualEnvs
+echo '- checking ~/VirtualEnvs for py27-virtualenvwrapper'
+if [ ! -d ~/VirtualEnvs ]; then
+    echo '++ not found. creating...'
+    mkdir -p ~/VirtualEnvs
+else
+    echo '++ exists. nothing to do...'
+fi
 
 # Download pathogen for vim
-curl -LSso ~/.vim/autoload/pathogen.vim \
-    https://tpo.pe/pathogen.vim
-
-# Download zenburn colorscheme for vim
-curl -Sso ~/.vim/colors/zenburn.vim \
-    http://slinky.imukuppi.org/zenburn/zenburn.vim
+echo '- checking for pathogen.vim'
+if [ ! -a ~/.vim/autoload/pathogen.vim ]; then
+    [ ! -a ~/.vim/autoload ] && mkdir -p ~/.vim/autoload
+    echo '++ downloading pathogen.vim'
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+else
+    echo '++ exists. nothing to do...'
+fi
 
 # Download Submodules
 git submodule init
-git submodule update
+git submodule update --recursive
 
 # Reload
 source $HOME/.profile
